6a9c936a008b964683d6ae4b613fffb8fddc186e
williansjaime <williansjaime@outlook.com>
2023-09-21 22:42:48 -0300

    Montagem das estruturas das funcoes concatenar

--- Medidor_Producao_Solar_ESP32/src/main.cpp
+++ Medidor_Producao_Solar_ESP32/src/main.cpp
@@ -4,6 +4,9 @@
 #include "SD.h"
 #include "SPI.h"
 #include <NTPClient.h> 
+#include <stdio.h>
+
+#define WORD_LENGTH 64
 
 const char *ssid = "nome_da_red";
 const char *password = "Senha_da_rede";
@@ -30,182 +33,10 @@ char valorEntrada;
 double Calcula_corrente();
 float Calcula_Tensao(int valor);
 float Calcula_Temperatura();
-void calcularCorrenteEsp32(int pino_sensor);
+float calcularCorrenteEsp32(int pino_sensor);
 
 WiFiUDP ntpUDP;
 NTPClient ntp(ntpUDP);
-
-void ListaDiretorios(fs::FS &fs, const char * dirname, uint8_t levels){
-  Serial.printf("Listing directory: %s\n", dirname);
-
-  File root = fs.open(dirname);
-  if(!root){
-    Serial.println("Failed to open directory");
-    return;
-  }
-  if(!root.isDirectory()){
-    Serial.println("Not a directory");
-    return;
-  }
-
-  File file = root.openNextFile();
-  while(file){
-    if(file.isDirectory()){
-      Serial.println("  DIR : ");
-      Serial.println(file.name());
-      if(levels){
-        ListaDiretorios(fs, file.name(), levels -1);
-      }
-    } else {
-      Serial.println("  FILE: ");
-      Serial.println(file.name());
-      Serial.println("  SIZE: ");
-      Serial.println(file.size());
-    }
-    file = root.openNextFile();
-  }
-}
-
-void createDir(fs::FS &fs, const char * path){
-  Serial.printf("Creating Dir: %s\n", path);
-  if(fs.mkdir(path)){
-    Serial.println("Dir created");
-  } else {
-    Serial.println("mkdir failed");
-  }
-}
-
-void removeDir(fs::FS &fs, const char * path){
-  Serial.printf("Removing Dir: %s\n", path);
-  if(fs.rmdir(path)){
-    Serial.println("Dir removed");
-  } else {
-    Serial.println("rmdir failed");
-  }
-}
-
-void readFile(fs::FS &fs, const char * path){
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if(!file){
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read from file: ");
-  while(file.available()){
-    Serial.write(file.read());
-  }
-  file.close();
-}
-
-void writeFile(fs::FS &fs, const char * path, const char * message){
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if(file.print(message)){
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-void appendFile(fs::FS &fs, const char * path, const char * message){
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if(!file){
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if(file.print(message)){
-      Serial.println("Message appended");
-  } else {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
-
-void renameFile(fs::FS &fs, const char * path1, const char * path2){
-  Serial.printf("Renaming file %s to %s\n", path1, path2);
-  if (fs.rename(path1, path2)) {
-    Serial.println("File renamed");
-  } else {
-    Serial.println("Rename failed");
-  }
-}
-
-void deleteFile(fs::FS &fs, const char * path){
-  Serial.printf("Deleting file: %s\n", path);
-  if(fs.remove(path)){
-    Serial.println("File deleted");
-  } else {
-    Serial.println("Delete failed");
-  }
-}
-
-void testFileIO(fs::FS &fs, const char * path){
-  File file = fs.open(path);
-  static uint8_t buf[512];
-  size_t len = 0;
-  uint32_t start = millis();
-  uint32_t end = start;
-  if(file){
-    len = file.size();
-    size_t flen = len;
-    start = millis();
-    while(len){
-      size_t toRead = len;
-      if(toRead > 512){
-        toRead = 512;
-      }
-      file.read(buf, toRead);
-      len -= toRead;
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes read for %u ms\n", flen, end);
-    file.close();
-  } else {
-    Serial.println("Failed to open file for reading");
-  }
-
-
-  file = fs.open(path, FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-
-  size_t i;
-  start = millis();
-  for(i=0; i<2048; i++){
-    file.write(buf, 512);
-  }
-  end = millis() - start;
-  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-  file.close();
-}
-
-
-void CardTipo(uint8_t cardType)
-{
-  if(cardType == CARD_MMC){
-    Serial.println("MMC");
-  } else if(cardType == CARD_SD){
-    Serial.println("SDSC");
-  } else if(cardType == CARD_SDHC){
-    Serial.println("SDHC");
-  } else {
-    Serial.println("UNKNOWN");
-  }
-}
-
 
 
 bool ConectarWifi(){
@@ -227,45 +58,11 @@ void setup(){
   pinMode(analogInputCorrentePainel,INPUT);
   pinMode(analogInputTensaoBatLition,INPUT);
   
-  /*if(!SD.begin(5)){
+  if(!SD.begin(5)){
     Serial.println("Card Mount Failed");
     return;
   }
-
-  uint8_t cardType = SD.cardType();
-  if(cardType == CARD_MMC){
-    Serial.println("MMC");
-  } else if(cardType == CARD_SD){
-    Serial.println("SDSC");
-  } else if(cardType == CARD_SDHC){
-    Serial.println("SDHC");
-  } else {
-    Serial.println("UNKNOWN");
-  }
- 
-  if(cardType == CARD_NONE){
-    Serial.println("No SD card attached");
-    return;
-  }
   
-  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-  ListaDiretorios(SD, "/", 0);
-  createDir(SD, "/mydir");
-  ListaDiretorios(SD, "/", 0);
-  removeDir(SD, "/mydir");
-  ListaDiretorios(SD, "/", 2);
-  writeFile(SD, "/hello.txt", "Hello ");
-  appendFile(SD, "/hello.txt", "World!\n");
-  readFile(SD, "/hello.txt");
-  deleteFile(SD, "/foo.txt");
-  renameFile(SD, "/hello.txt", "/foo.txt");
-  readFile(SD, "/foo.txt");
-  testFileIO(SD, "/test.txt");
-  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
- */
 }
 
 void loop(){
@@ -306,23 +103,33 @@ void loop(){
         delay(10000);
     }
      **/
-    Serial.print("Corrente::");
+    char InputCorrentePainel[WORD_LENGTH];
+    sprintf(InputCorrentePainel, "%.2f", calcularCorrenteEsp32(analogInputCorrentePainel));
+    Serial.println(InputCorrentePainel);
+    char InputTensaoBateria[WORD_LENGTH];
+    sprintf(InputTensaoBateria, "%.2f", Calcula_Tensao(analogRead(analogInputTensaoBateria)));
+    Serial.println(InputTensaoBateria);
+    char TensaoPainel[WORD_LENGTH];
+    sprintf(TensaoPainel, "%.2f", Calcula_Tensao(analogRead(analogInputTensaoPainel)));
+    Serial.println(TensaoPainel);
+    char TensaoBatLition[WORD_LENGTH];    
+    sprintf(TensaoBatLition, "%.2f", Calcula_Tensao(analogRead(analogInputTensaoBatLition)));
+    Serial.println(TensaoBatLition);    
+    delay(1000);
+    
+    /*Serial.println(InputCorrentePainel);
+    Serial.print("InputTensaoBateria::%s",InputTensaoBateria);
+    Serial.print("TensaoPainel::%s",TensaoPainel);*/
+
+    /*
     calcularCorrenteEsp32(analogInputCorrentePainel);
     Serial.println("Tensao::");
     Serial.println(Calcula_Tensao(analogRead(analogInputTensao)));
-    Serial.println(Calcula_Tensao(analogRead(analogInputTensaoBateria)));
-    Serial.println(Calcula_Tensao(analogRead(analogInputTensaoPainel)));
-    Serial.println(Calcula_Tensao(analogRead(analogInputTensaoBatLition)));
     Serial.println("Temperatura::");
     Serial.println(Calcula_Temperatura(analogInputTemperatura));
-    delay(1000); 
-    char InputTensaoBateria[] = (char *)Calcula_Tensao(analogRead(analogInputTensaoBateria));
-    char *b = (char *)Calcula_Tensao(analogRead(analogInputTensaoPainel));
-    char *c = (char *)Calcula_Tensao(analogRead(analogInputTensaoBatLition));
-    char *d = (char *)Calcula_corrente(); 
-    char *e = (char *)Calcula_Temperatura();        
-    //Serial.println("/TensaoBateria: "+a+"/TensaoPainel: "+b+"/TensaoBatLition: "+c+"/Calcula_corrente: "+d+"/Calcula_Temperatura: "+e+"/");   
+    */
 
+    /*
     if (SD.begin()) { 
         myFile = SD.open("dadosSolar.txt", FILE_WRITE); // Cria / Abre arquivo .txt
         if (myFile) { // Se o Arquivo abrir imprime:
@@ -333,7 +140,7 @@ void loop(){
             Serial.println("Erro cartao");
         } 
         myFile.close(); 
-    }    */
+    }*/ 
 }
 
 //Testado
@@ -382,7 +189,7 @@ float Calcula_Temperatura(int pino_input_temperatura)
     return ReturnT;
 }
 
-void calcularCorrenteEsp32(int pino_sensor){
+float calcularCorrenteEsp32(int pino_sensor){
   int menor_valor;
   int valor_lido;
   int menor_valor_acumulado = 0;
@@ -415,9 +222,6 @@ void calcularCorrenteEsp32(int pino_sensor){
     }
     delayMicroseconds(10);  
   }
-  
-  Serial.print("Menor Valor:");
-  Serial.println(menor_valor);
 
   //Transformar o maior valor em corrente de pico
   corrente_pico = ZERO_SENSOR - menor_valor; // Como o ZERO do sensor é 2,5 V, é preciso remover este OFFSET. Na leitura Analógica do ESp32 com este sensor, vale 2800 (igual a 2,5 V).
@@ -427,11 +231,9 @@ void calcularCorrenteEsp32(int pino_sensor){
  
   //Converter para corrente eficaz  
   corrente_eficaz = corrente_pico/1.4;
-  Serial.print("Corrente Eficaz:");
+  /*Serial.print("Corrente Eficaz:");
   Serial.print(corrente_eficaz);
-  Serial.print(" A");
-  Serial.print(" --- ");
   Serial.print(corrente_eficaz*1000);
-  Serial.println(" mA");
-
+  Serial.println(" mA");*/
+  return corrente_eficaz;
 }
